f47a91c73f5a3106d3335f46dc1d20c44758fae7
“sarthak-gupta229Git config --global user.email “guptasarthak229@gmail.com <guptasarthak229@gmail.com>
2026-06-17 16:42:25 +0530

    feat: implement project CRUD operations

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -7,7 +7,7 @@ import { asyncHandler } from "../utils/async-handler.js";
 import mongoose from "mongoose";
 import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 
-const addMembersToProject = asyncHandler(async (req, res) => {
+const getProjects = asyncHandler(async (req, res) => {
   const projects = await ProjectMember.aggregate([
     {
       $match: {
@@ -98,21 +98,163 @@ const updateProject = asyncHandler(async (req, res) => {
 });
 
 const deleteMember = asyncHandler(async (req, res) => {
+  const { projectId, userId } = req.params;
+  const projectMember = await Project.findOne({
+    project: new mongoose.Types.ObjectId(projectId),
+    user: new mongoose.Types.ObjectId(userId),
+  });
+  if (!projectMember) {
+    throw new ApiError(400, "Project member not found");
+  }
+  projectMember = await ProjectMember.findByIdAndDelete(projectMember._id);
+  if (!projectMember) {
+    throw new ApiError(400, "Project member not found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        projectMember,
+        "Project member deleted successfully",
+      ),
+    );
+});
+
+const getProjectById = asyncHandler(async (req, res) => {
   const { projectId } = req.params;
-  const project = await Project.findByIdAndDelete(projectId);
+  const project = await Project.findById(projectId);
   if (!project) {
     throw new ApiError(404, "Project not found");
   }
   return res
     .status(200)
-    .json(new ApiResponse(200, project, "Project deleted successfully"));
+    .json(new ApiResponse(200, project, "Project fetched successfully"));
 });
-const getProjects = asyncHandler(async (req, res) => {});
-const getProjectById = asyncHandler(async (req, res) => {});
-const getProjectMembers = asyncHandler(async (req, res) => {});
-const deleteProject = asyncHandler(async (req, res) => {});
-const updateMemberRole = asyncHandler(async (req, res) => {});
 
+const addMembersToProject = asyncHandler(async (req, res) => {
+  const { email, role } = req.body;
+  const { projectId } = req.params;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(404, "User does not exists");
+  }
+
+  await ProjectMember.findByIdAndUpdate(
+    projectId,
+    {
+      user: new mongoose.Types.ObjectId(user._id),
+      project: new mongoose.Types.ObjectId(projectId),
+    },
+    {
+      user: new mongoose.Types.ObjectId(user._id),
+      project: new mongoose.Types.ObjectId(projectId),
+      role: role,
+    },
+    {
+      new: true,
+      upsert: true,
+    },
+  );
+});
+
+const deleteProject = asyncHandler(async (req, res) => {
+  const { projectId } = req.params();
+
+  const project = await Project.findByIdAndDelete(projectId);
+  if (!project) {
+    throw new ApiError(404, "Project does not exists");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project deleted Sucessfully"));
+});
+
+const updateMemberRole = asyncHandler(async (req, res) => {
+  const { userId, projectId } = req.params;
+  const { newRole } = req.body;
+  if (!AvailableUserRole.includes(newRole)) {
+    throw new ApiError(400, "Invalid Role");
+  }
+  let projectMember = await ProjectMember.findOne({
+    project: new mongoose.Types.ObjectId(projectId),
+    user: new mongoose.Types.ObjectId(userId),
+  });
+  if (!projectMember) {
+    throw new ApiError(400, "Project member not found");
+  }
+  projectMember = await ProjectMember.findByIdAndUpdate(
+    projectMember._id,
+    {
+      role: newRole,
+    },
+    { new: true },
+  );
+  if (!projectMember) {
+    throw new ApiError(400, "Project member not found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        projectMember,
+        "Project member role updated successfully",
+      ),
+    );
+});
+
+const getProjectMembers = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
+  const project = await Project.findById(req.params);
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+  const projectMembers = new Project.aggregate([
+    {
+      $match: mongoose.Types.ObjectId(projectId),
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        as: "user",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        user: {
+          $arrayElemAt: ["$user", 0],
+        },
+      },
+    },
+    {
+      $project: {
+        project: 1,
+        user: 1,
+        role: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        _id: 0,
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, projectMembers, "Project members fetched"));
+});
 export {
   addMembersToProject,
   createProject,
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -26,3 +26,36 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     throw new ApiError(401, "Invalid access token");
   }
 });
+
+
+export const validateProjectPermission = (roles = []) => {
+  asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw new ApiError(400, "project id is missing");
+    }
+
+    const project = await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user._id),
+    });
+
+    if (!project) {
+      throw new ApiError(400, "project not found");
+    }
+
+    const givenRole = project?.role;
+
+    req.user.role = givenRole;
+
+    if (!roles.includes(givenRole)) {
+      throw new ApiError(
+        403,
+        "You do not have permission to perform this action",
+      );
+    }
+
+    next();
+  });
+};
